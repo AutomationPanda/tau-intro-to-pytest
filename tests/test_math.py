58e122e179e8169a7b16a47f7ebb26d2a6fbbac7
AndyLPK247 <AndyLPK247@gmail.com>
2020-06-12 17:34:56 -0400

    Chapter 2 example code

--- tests/test_math.py
+++ tests/test_math.py
@@ -9,3 +9,14 @@ Their purpose is to show how to use the pytest framework by example.
 
 def test_one_plus_one():
   assert 1 + 1 == 2
+
+
+# --------------------------------------------------------------------------------
+# A test function to show assertion introspection
+# --------------------------------------------------------------------------------
+
+def test_one_plus_two():
+  a = 1
+  b = 2
+  c = 3
+  assert a + b == c
