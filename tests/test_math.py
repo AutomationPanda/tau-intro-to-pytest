6172872e80965d04e73d87923dd9013770495462
AndyLPK247 <AndyLPK247@gmail.com>
2020-06-14 11:22:57 -0400

    Chapter 4 example code

--- tests/test_math.py
+++ tests/test_math.py
@@ -38,3 +38,21 @@ def test_divide_by_zero():
     num = 1 / 0
   
   assert 'division by zero' in str(e.value)
+
+
+# --------------------------------------------------------------------------------
+# A parametrized test function
+# --------------------------------------------------------------------------------
+
+products = [
+  (2, 3, 6),            # postive integers
+  (1, 99, 99),          # identity
+  (0, 99, 0),           # zero
+  (3, -4, -12),         # positive by negative
+  (-5, -5, 25),         # negative by negative
+  (2.5, 6.7, 16.75)     # floats
+]
+
+@pytest.mark.parametrize('a, b, product', products)
+def test_multiplication(a, b, product):
+  assert a * b == product
