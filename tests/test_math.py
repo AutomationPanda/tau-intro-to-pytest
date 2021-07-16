8b73e37821c66c6d75c00de610f082e69cc95990
Mayank Baranwal <mayank.baranwal1794@gmail.com>
2021-07-17 01:00:55 +0530

    Added comments as per the video lecture

--- tests/test_math.py
+++ tests/test_math.py
@@ -11,7 +11,7 @@ import pytest
 
 
 # --------------------------------------------------------------------------------
-# A most basic test function
+# A most basic test function - Chapter 1 - The First Test Case
 # --------------------------------------------------------------------------------
 
 @pytest.mark.math
@@ -20,7 +20,7 @@ def test_one_plus_one():
 
 
 # --------------------------------------------------------------------------------
-# A test function to show assertion introspection
+# A test function to show assertion introspection - Chapter 2 -  Failing Test Case
 # --------------------------------------------------------------------------------
 
 @pytest.mark.math
@@ -32,11 +32,15 @@ def test_one_plus_two():
 
 
 # --------------------------------------------------------------------------------
-# A test function that verifies an exception
+# A test function that verifies an exception - Chapter 3 - A Test Case with an Exception
 # --------------------------------------------------------------------------------
 
 @pytest.mark.math
 def test_divide_by_zero():
+  # In Python, "with" is a special statement for automatically handling extra "enter" and "exit" logic for a caller.
+  # It's most commonly used for File Input and Output. The "enter" logic opens the file body reads or writes 
+  # and the "exit" logic closes the file. For "pytest.raises" the "enter" logic makes the code to catch any exceptions 
+  # and "exit" logic asserts if the desired exception type was actually raised.
   with pytest.raises(ZeroDivisionError) as e:
     num = 1 / 0
   
@@ -44,10 +48,12 @@ def test_divide_by_zero():
 
 
 # --------------------------------------------------------------------------------
-# A parametrized test function
+# A parametrized test function - Chapter 4 - Parameterized Test Case 
+# DRY Principle :- Don't Repeat Yourself! 
+# https://en.wikipedia.org/wiki/Don%27t_repeat_yourself
 # --------------------------------------------------------------------------------
 
-products = [
+products = [            # Below comments are the Multiplication Test Ideas
   (2, 3, 6),            # postive integers
   (1, 99, 99),          # identity
   (0, 99, 0),           # zero
@@ -56,7 +62,19 @@ products = [
   (2.5, 6.7, 16.75)     # floats
 ]
 
+
 @pytest.mark.math
+# https://docs.pytest.org/en/6.2.x/parametrize.html
+# @pytest.mark.parametrize - is a decorator for the - test_multiplication() function.
+# In Python, a decorator is a special function that wraps around another functon.
+# We need to pass 2 functions into the decorator - the first argument is string containing the comma seperated list of variable names. 
+# These parameters must match the parameter (here in this exmaple - a, b, product) names for the Test Case function.
+
+# Pytest Parameters make it easy to do the data driven testing in which the same Test cases cranck through multiple inputs. This Test case 
+# is just a basic example that what you can do with paramaters. We can use any python object for parameter values not just integers. 
+# Sicne parameters are passed into the test cases as a list, we could also store the data into the external files like csv's or excel 
+# spreadhseets and read them when the test runs. 
 @pytest.mark.parametrize('a, b, product', products)
+# Inner function - is the Test Case. 
 def test_multiplication(a, b, product):
   assert a * b == product
