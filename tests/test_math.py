961ccad5b43b70576b9dc2780f7257c2f866a253
Pandy Knight <andylpk.247@gmail.com>
2023-05-20 16:39:55 -0400

    Wrote first test module

---
+++ tests/test_math.py
@@ -0,0 +1,11 @@
+"""
+This module contains basic unit tests for math operations.
+Their purpose is to show how to use the pytest framework by example.
+"""
+
+# --------------------------------------------------------------------------------
+# A most basic test function
+# --------------------------------------------------------------------------------
+
+def test_one_plus_one():
+  assert 1 + 1 == 2
