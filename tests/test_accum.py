c591528192565c5316f2ebac1aab8c721dce0f91
Pandy Knight <andylpk.247@gmail.com>
2023-05-20 20:10:59 -0400

    Rewrote accum tests with a fixture

--- tests/test_accum.py
+++ tests/test_accum.py
@@ -12,34 +12,38 @@ from stuff.accum import Accumulator
 
 
 # --------------------------------------------------------------------------------
+# Fixtures
+# --------------------------------------------------------------------------------
+
+@pytest.fixture
+def accum():
+  return Accumulator()
+
+
+# --------------------------------------------------------------------------------
 # Tests
 # --------------------------------------------------------------------------------
 
-def test_accumulator_init():
-  accum = Accumulator()
+def test_accumulator_init(accum):
   assert accum.count == 0
 
 
-def test_accumulator_add_one():
-  accum = Accumulator()
+def test_accumulator_add_one(accum):
   accum.add()
   assert accum.count == 1
 
 
-def test_accumulator_add_three():
-  accum = Accumulator()
+def test_accumulator_add_three(accum):
   accum.add(3)
   assert accum.count == 3
 
 
-def test_accumulator_add_twice():
-  accum = Accumulator()
+def test_accumulator_add_twice(accum):
   accum.add()
   accum.add()
   assert accum.count == 2
 
 
-def test_accumulator_cannot_set_count_directly():
-  accum = Accumulator()
+def test_accumulator_cannot_set_count_directly(accum):
   with pytest.raises(AttributeError, match=r"property 'count' of 'Accumulator' object has no setter") as e:
     accum.count = 10
